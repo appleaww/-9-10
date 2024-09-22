dbd28f3ad15e946b4e9b419119c8df482806cb63
appleaww <artemmorozz2006@gmail.com>
2024-09-22 16:26:49 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,11 +4,14 @@ int main()
 {
 int a;
 int b;
+int c;
 std::cout << "Введите число a\n";
 std::cin >>a;
+std::cout << "Введите число c\n";
+std::cin >>c;
 std::cout << "Введите число b\n";
 std::cin >>b;
-std::cout << "Сумма чисел равна " << a+b << "\n";
-std::cout << "Разность чисел равна " << a-b;
+std::cout << "Сумма чисел равна " << a+b+c << "\n";
+std::cout << "Разность чисел равна " << a-b-c;
 return 0;
 }
