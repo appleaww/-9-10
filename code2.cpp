1ec5086351edb4f97b4406f3827098ab4aa95129
appleaww <artemmorozz2006@gmail.com>
2024-09-22 16:25:20 +0300

    Update code2.cpp

--- code2.cpp
+++ code2.cpp
@@ -4,10 +4,13 @@ int main()
 {
 int a;
 int b;
+int c;
 std::cout << "Введите число a\n";
 std::cin >>a;
 std::cout << "Введите число b\n";
 std::cin >>b;
-std::cout << "Произведение чисел равно " << a*b << "\n";
+std::cout << "Введите число c\n";
+std::cin >>c;
+std::cout << "Произведение чисел равно " << a*b*c << "\n";
 return 0;
 }
